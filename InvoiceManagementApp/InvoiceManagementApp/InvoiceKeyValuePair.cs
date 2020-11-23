9a2e0e58df005815837d4baddd36a9d8435c130b
Emir Jasarevic <emir2000@hotmail.se>
2020-11-24 00:01:37 +0100

    added project

---
+++ InvoiceManagementApp/InvoiceManagementApp/InvoiceKeyValuePair.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementApp
+{
+    public class InvoiceKeyValuePair
+    {
+        public readonly string Key;
+        public readonly string Value;
+        public InvoiceKeyValuePair(string key, string value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+        public override int GetHashCode()
+        {
+            return Key.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj != null && obj is InvoiceKeyValuePair ikvp ? Key == ikvp.Key : false;
+        }
+    }
+}
---
+++ InvoiceManagementApp/InvoiceManagementApp/Manager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+
+namespace InvoiceManagementApp
+{
+    public class Manager : IEnumerable<InvoiceKeyValuePair>
+    {
+        private HashSet<InvoiceKeyValuePair> hashSet = new HashSet<InvoiceKeyValuePair>();
+
+        public void LoadFromFile(string FilePath)
+        {
+            if (FilePath != null && File.Exists(FilePath))
+            {
+                String line;
+                using (StreamReader sr = new StreamReader(FilePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        line = sr.ReadLine();
+                        var array = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                        foreach (var item in array)
+                        {
+                            string invoiceNumber = Path.GetFileNameWithoutExtension(item);
+                            Add(invoiceNumber, item);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Add(string key, string line)
+        {
+            hashSet.Add(new InvoiceKeyValuePair(key, line));
+        }
+
+        public string FindURL (string key)
+        {
+            if (hashSet.TryGetValue(new InvoiceKeyValuePair(key, ""), out InvoiceKeyValuePair value))
+                return value.Value;
+            else
+                return null;
+        }
+
+        public IEnumerator<InvoiceKeyValuePair> GetEnumerator()
+        {
+            return hashSet.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return hashSet.GetEnumerator();
+        }
+    }
+}
